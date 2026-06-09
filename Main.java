84f90d4fb129a7972f3f263e61bbf6523438ba04
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-09 15:56:28 +0300

    Day 1 learnt how to write my OOP simple program

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+public static void main (String [] args){
+    Book book = new Book();
+   // System.out.println(book.title);
+    //System.out.println(book.author);
+    //System.out.println(book.publisher);
+    //System.out.println(book.publicationyear);
+    //System.out.println(book.price);
+    //System.out.println(book.quantity);
+    //System.out.println(book.isInteresting);
+   book.read();
+   book.bought();
+   book.comment(); 
+}    
+}
