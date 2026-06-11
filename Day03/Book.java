ed1f21df7caac74fafee1c7e0169d57d0eb5eff0
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-11 19:00:52 +0300

    Applied Overloaded Constructors

---
+++ Day03/Book.java
@@ -0,0 +1,34 @@
+package Day03;
+
+public class Book {
+    String name;
+    String author;
+    String publisher;
+    int publicationYear;
+    
+    
+Book(String name){
+    this.name= name;
+    this.author="Ken Walibora";
+    this.publisher="Longhorn Publishers";
+    this.publicationYear=1996;
+}
+Book(String name, String author){
+    this.name= name;
+    this.author=author;
+    this.publisher="Longhorn Publishers";
+    this.publicationYear=1996;
+}
+Book(String name, String author, String publisher){
+    this.name= name;
+    this.author=author;
+    this.publisher=publisher;
+    this.publicationYear=1996;
+}
+Book(){
+    this.name="Gone Girl";
+    this.author="Fillian Flynn";
+    this.publisher="Crown Publishing Group";
+    this.publicationYear=2012;
+}
+}
---
+++ Day03/day3.md
@@ -0,0 +1,3 @@
+# Overloaded Constructors
+Allows a class to have multiple constructors with diffe parameters
+Enables object to be initialized in various ways.
