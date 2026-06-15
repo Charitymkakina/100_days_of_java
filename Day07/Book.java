c44ae52134e3c29e72d36ae9d170b8f2d82dc71f
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-15 16:35:35 +0300

    Inheritance

---
+++ Day07/Book.java
@@ -0,0 +1,25 @@
+package Day07;
+
+public class Book {
+    protected String title;
+    protected String author;
+    protected String publisher;
+    protected int publicationYear;
+
+public Book(String title,String author, String publisher, int publicationYear){
+    this.title=title;
+    this.author=author;
+    this.publisher=publisher;
+    this.publicationYear=publicationYear;
+
+}
+public void displayInfo(){
+    System.out.println("Title: " + title);
+    System.out.println("Author: " + author);
+    System.out.println("Publisher: " + publisher);
+    System.out.println("Year: " +  publicationYear);
+}
+public void read(){
+    System.out.println("I am currently reading " + this.title);
+}
+}
---
+++ Day07/day7.md
@@ -0,0 +1,7 @@
+# Inhritance
+One class (sub class) inherits the attributes and methods of another class(Super class)
+
+To inherit from a class use the keyword `extends`
+
+`final` keyword is used when you do not want other classes to inherit from a class.
+
