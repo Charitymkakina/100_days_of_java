c8f7d7871982a5025724a004cbdf2c13be8a33b2
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-20 13:31:49 +0300

    Added comments

--- Day10/CreativeNonFiction.java
+++ Day10/CreativeNonFiction.java
@@ -1,6 +1,8 @@
 package Day10;
 
-public class CreativeNonFiction implements Fiction, NonFiction{
+public class CreativeNonFiction implements Fiction, NonFiction{ // To acheve multiple inheritance behaviour,
+// java allows a class to have multiple interfaces
+
     @Override
     public void Fantasy(){
         System.out.println(" * The Fairy Tellers * Reading this book gives a fantasy feel");
