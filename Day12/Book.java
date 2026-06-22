2e18336a7fcecc6340fd8098e8dbabd1b8d3fee1
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-22 12:16:15 +0300

    Added comments

--- Day12/Book.java
+++ Day12/Book.java
@@ -1,8 +1,9 @@
 package Day12;
 
-public enum Book {
-   ROMANCE(1), FANTASY(2), SCIFI(3), SELFHELP(4), TRUECRIME(5);
-   
+public enum Book { //To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma.
+   ROMANCE(1), FANTASY(2), SCIFI(3), SELFHELP(4), TRUECRIME(5); // Constants should all be in uppercase.
+
+// An enum can also have a constructor just like a class
 private final int bookNumber;
 Book(int bookNumber){
     this.bookNumber = bookNumber;
--- Day12/day12.md
+++ Day12/day12.md
@@ -1,1 +1,2 @@
 # Java ENUM
+`Enum` is a shortform for `Enumeration` which means specifically listed.
