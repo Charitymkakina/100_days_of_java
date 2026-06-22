7ccfba6a38a5de250ba2ca298189d43e6c956237
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-22 18:30:01 +0300

    Added exception handling

--- Day12/Main.java
+++ Day12/Main.java
@@ -7,13 +7,20 @@ public class Main {
         Scanner scanner = new Scanner (System.in);
         System.out.println("Enter the genre you want to read: ");
         String response = scanner.nextLine().toUpperCase();
-        Book book = Book.valueOf(response);
+        try{
+           Book book = Book.valueOf(response);
 
         switch (book) {
             case ROMANCE, FANTASY, SCIFI -> System.out.println("This is a fiction based book");
-            case SELFHELP, TRUECRIME -> System.out.println("This is a non-fiction based book");
-        
+
+            case SELFHELP, TRUECRIME -> System.out.println ("This is a non-fiction based book") ;
+         } 
         }
+        catch(IllegalArgumentException e){
+            System.out.println("Please Enter a valid Book Genre! ");
+        } 
+
+    
         scanner.close();
     }
 }
