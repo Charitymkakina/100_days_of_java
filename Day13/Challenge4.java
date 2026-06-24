b99bbe3073eea644d891892af04efd9cf8151f6c
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-24 13:51:28 +0300

    Stack solved challenges

---
+++ Day13/Challenge4.java
@@ -0,0 +1,22 @@
+package Day13;
+//Create a stack of books, push 5 books after every push print currently reading: <top book>
+
+import java.util.Stack;
+public class Challenge4 {
+    public static void main (String []args){
+        Stack <String> stack = new Stack<>();
+        stack.push("Siku Njema");
+        System.out.println("Currently Reading: " + stack.peek());
+        stack.push("Blossoms");
+        System.out.println("Currently Reading: " + stack.peek());
+        stack.push("Chozi la Heri");
+        System.out.println("Currently Reading: " + stack.peek());
+        stack.push("Inheritance");
+        System.out.println("Currently Reading: " + stack.peek());
+        stack.push("A dolls House");
+        System.out.println("Currently Reading: " + stack.peek());
+        //stack.pop();
+        //System.out.println("Currently Reading: " + stack.peek());
+        
+    }
+}
---
+++ Day13/Challenge6.java
@@ -0,0 +1,17 @@
+package Day13;
+//A user decides to read Book A, Book B, Book C, Book D. the decides to undo the last action twice. Print the current book
+
+import java.util.Stack;
+public class Challenge6 {
+    public static void main (String[]args){
+        Stack <String> stack = new Stack <String>();
+
+        for (char c= 'A'; c<='D'; c++){
+            stack.push("Book " + c);
+        }
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
+    }
+}
