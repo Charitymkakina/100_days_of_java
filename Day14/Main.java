07bb473b6ff83d993cfb79b77b3ba100c43f61f0
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-24 21:31:39 +0300

    Queue

--- Day14/Main.java
+++ Day14/Main.java
@@ -1,5 +1,20 @@
 package Day14;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
+    public static void main (String []args){
+        Queue <String> queue = new LinkedList<>();
     
+        queue.offer("Charity");
+        queue.offer("Mkakina");
+        queue.offer("Neema");
+        queue.offer("Malemba");
+        queue.offer("Vanessa");
+
+        queue.poll();
+    System.out.println(queue.peek());
+    System.out.println(queue);
+    }
 }
