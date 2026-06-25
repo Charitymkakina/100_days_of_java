f66d374f2bf28d82858770b5ed02e98c9a5b89df
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-25 21:50:00 +0300

    Comments

--- Day14/Main.java
+++ Day14/Main.java
@@ -4,17 +4,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    public static void main (String []args){
-        Queue <String> queue = new LinkedList<>();
-    
+    public static void main(String[] args) {
+
+        // Create a Queue using a LinkedList implementation.
+        // Queues follow the FIFO (First In, First Out) principle.
+        Queue<String> queue = new LinkedList<>();
+
+        // Adding elements to the queue using offer().
+        // Elements are added to the rear (end) of the queue.
         queue.offer("Charity");
         queue.offer("Mkakina");
         queue.offer("Neema");
         queue.offer("Malemba");
         queue.offer("Vanessa");
 
+        // Removes the element at the front of the queue.
+        // Since queues are FIFO, "Charity" is removed first.
         queue.poll();
-    System.out.println(queue.peek());
-    System.out.println(queue);
+
+        // Displays the element at the front of the queue
+        // without removing it.
+        System.out.println(queue.peek());
+
+        // Prints all elements currently in the queue.
+        System.out.println(queue);
     }
 }
