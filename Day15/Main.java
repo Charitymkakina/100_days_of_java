0b6568e7185680326691d5f817a92ccdc8b432f9
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-26 15:50:53 +0300

    Linkedlist treated as queues

--- Day15/Main.java
+++ Day15/Main.java
@@ -5,11 +5,16 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList <String> linkedList = new LinkedList<>();
-        linkedList.push("Charity"); //LinkedList can be treatd as a stack
-        linkedList.push("Mkakina");
-        linkedList.push("Neema");
+        //linkedList.push("Charity"); //LinkedList can be treatd as a stack
+        //linkedList.push("Mkakina");
+        //linkedList.push("Neema");
 
-        linkedList.poll();
+       // linkedList.pop();
+
+       linkedList.offer("Charity");
+       linkedList.offer("Mkakina");
+       linkedList.offer("Neema");
+       //linkedList.poll();
         System.out.println(linkedList);
     }
 }
