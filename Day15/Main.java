65ddbf0e1c9d11f38396ce30a309e74305a9d4c1
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-26 15:47:19 +0300

    Linkedlist treated as stack

---
+++ Day15/Main.java
@@ -0,0 +1,15 @@
+package Day15;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList <String> linkedList = new LinkedList<>();
+        linkedList.push("Charity"); //LinkedList can be treatd as a stack
+        linkedList.push("Mkakina");
+        linkedList.push("Neema");
+
+        linkedList.poll();
+        System.out.println(linkedList);
+    }
+}
