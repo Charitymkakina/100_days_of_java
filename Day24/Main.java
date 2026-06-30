5db925f68a101a291a3cc4a24e142c8b9e56648a
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-30 20:28:20 +0300

    Recursion

---
+++ Day24/Main.java
@@ -0,0 +1,17 @@
+package Day24;
+
+public class Main {
+   public static void main(String[] args) {
+    read(5);
+   }
+   private static void read(int chapter){
+   // for (int i=0; i< steps; i++){ // Iteration which is a repetition of a process
+       // System.out.println("You take a step!");
+       if (chapter < 1) return; //base case
+       System.out.println("Read a chapter a day!");
+       read(chapter - 1); // Recursive case
+    }
+   } 
+
+  
+
