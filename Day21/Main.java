b2fda0f892dd14df59fbdc87d74b9b63bd5e58e8
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-30 18:53:39 +0300

    Selection Sort

--- Day21/Main.java
+++ Day21/Main.java
@@ -2,7 +2,7 @@ package Day21;
 
 public class Main{
     public static void main (String [] args){
-        int array[] = {9,1,8,2,7,3,6,4,5};
+        int array[] = {9,1,8,2,7,3,6,4,5}; 
 
         bubbleSort(array);
 
@@ -13,7 +13,7 @@ public class Main{
     public static void bubbleSort(int array[]){
         for(int i=0; i<array.length-1;i++){
             for(int j=0; j<array.length-i-1;j++){
-                if(array[j] > array[j+1]){
+                if(array[j] > array[j+1]){ // Ensures the array is displayed in a ascending order.
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
