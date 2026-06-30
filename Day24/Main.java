7da8089d8ded10189e28b585c1658e254dc7446d
Charity Mkakina <charitymkakina1@gmail.com>
2026-06-30 21:26:25 +0300

    comment

--- Day24/Main.java
+++ Day24/Main.java
@@ -5,8 +5,8 @@ public class Main {
     read(5);
    }
    private static void read(int chapter){
-   // for (int i=0; i< steps; i++){ // Iteration which is a repetition of a process
-       // System.out.println("You take a step!");
+   // for (int i=0; i< chapter; i++){ // Iteration which is a repetition of a process
+       // System.out.println("Read a chapter!");
        if (chapter < 1) return; //base case
        System.out.println("Read a chapter a day!");
        read(chapter - 1); // Recursive case
