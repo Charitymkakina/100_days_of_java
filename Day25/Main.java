a428b082ff4de14a66b358b5bd99a59455dd2c92
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-01 16:21:58 +0300

    Mergesort

--- Day25/Main.java
+++ Day25/Main.java
@@ -2,6 +2,40 @@ package Day25;
 
 public class Main {
     public static void main(String[] args) {
+        int [] array= {8,3,5,3,4,7,6,1};
+        mergeSort(array);
         
+        
+
+        for (int i=0; i<array.length;i++){
+            System.out.println(array[i] + " ");
+        }
+}
+private static void mergeSort(int[]array){
+    int length = array.length;
+    if (length <= 1) return; //base case
+
+    int middle = length/2;
+    int[] leftArray= new int[middle];
+    int[] rightArray= new int[length-middle];
+
+    int i = 0; //left array
+    int j = 0; //right array
+
+    for (; i < length; i++ ){
+        if (i<middle){
+            leftArray[i]= array[i];
+        }
+        else {
+        rightArray[j]= array[i];
+        j++;
+        }
+    }
+    mergeSort(leftArray);
+    mergeSort(rightArray);
+    merge(leftArray, rightArray, array);
+}
+private static void merge(int[] leftArray, int[] rightArray, int[]array){
+
 }
 }
