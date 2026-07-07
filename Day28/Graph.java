620cce70214145deef2b14d41c2f99cb8f8e8169
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-07 11:57:11 +0300

    Adjacency Matrix

---
+++ Day28/Graph.java
@@ -0,0 +1,40 @@
+package Day28;
+
+import java.util.ArrayList;
+
+public class Graph {
+    ArrayList<Node> nodes; // Addidng headers to our Adjacency matrix.
+
+     int[][] matrix;
+    Graph (int size) { // the amount of nodes we might have.
+        nodes = new ArrayList<>();
+        matrix = new int[size] [size]; // No. of vertices squared.
+    }
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+    public void addEdge(int src, int dst){ // src is the row and dst is he column
+        matrix[src][dst] = 1;
+    }
+    public boolean checkEdge(int src, int dst){
+        if ( matrix[src][dst] == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void print(){
+        for (Node node : nodes ){
+            System.out.print(node.data + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++){ // will iterate over all the matrices
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
