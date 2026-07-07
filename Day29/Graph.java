54cf2921bb931c6cb803d7ff83c3ac12799a41ce
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-07 14:44:15 +0300

    Adjacency List

--- Day29/Graph.java
+++ Day29/Graph.java
@@ -8,16 +8,34 @@ public class Graph {
     Graph(){
         alist = new ArrayList<>();
     }
-    public void addNode(Node node){
-
+    public void addNode(Node node){ 
+        LinkedList<Node> currentList = new LinkedList<>(); // when we create a new node we create a new LinkedList.
+        currentList.add(node);
+        alist.add(currentList);
     }
     public void addEdge(int src, int dst){
-
+        LinkedList<Node> currentList = alist.get(src); // returns a linkedList.
+        Node dstNode = alist.get(dst).get(0); //dstNode is the address of the node we would like to link to.
+        currentList.add(dstNode);
     }
-    public void checkEdge(int src, int dst){
+    public boolean checkEdge(int src, int dst){
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).get(0);
+        for (Node node : currentList){
+            if (node == dstNode){
+                return true;
+            }
 
+        }
+        return false; 
     }
     public void print(){
-        
+        for (LinkedList<Node> currentList : alist){
+            for (Node node : currentList){
+                System.out.print(node.data + " -> ");
+            }
+            System.out.println();
+
+        }
     }
 }
