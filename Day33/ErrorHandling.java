8b2b41546c6eeb34774eba0383b2a8f0d9cf06d4
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-15 17:12:33 +0300

    Exception handling

---
+++ Day33/ErrorHandling.java
@@ -0,0 +1,28 @@
+package Day33;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ErrorHandling {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        try{
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println(number);
+        }
+        catch(InputMismatchException e){ // catching a mismatch of the data type if user puts another datatype instead of integer.
+            System.out.println("Use an integer value!");
+        }
+        catch(ArithmeticException e ){
+            System.out.println("You can't divide by zero!");
+        }
+        catch(Exception e){ // when all exceptions fails.
+            System.out.println("Something went wrong, try again!");
+        }
+        finally{ // Always executes whether there is an exception or not.
+            scanner.close();
+        }
+    }
+}
