42a4071c505bf75343e782a64128e0ef7cd0781a
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-19 15:36:36 +0300

    Spring-boot

--- demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloController{
 
-    @GetMapping("/")
+    @GetMapping("/") // maps a url to a java method
     public String Hello(){
         return "Hello";
     }
