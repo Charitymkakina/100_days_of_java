1f2298968fbb282fd981d6ffe061662472ef5e9d
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-23 22:03:37 +0300

    HTTP Methods

--- demo/src/main/java/com/example/demo/controllers/BookController.java
+++ demo/src/main/java/com/example/demo/controllers/BookController.java
@@ -1,22 +1,47 @@
-package com.example.demo.controllers;
-
+package com.example.demo.controllers; 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class BookController {
 
     boolean available = true;
 
-    @GetMapping("/borrow")
-    public String borrow(@RequestParam String book) {
-
-        if (available) {
-            available = false;
-            return "You have borrowed " + book + " successfully.";
+    @GetMapping("/book")
+    public String getBook(@RequestParam String book){
+        if(available){
+            return book + " is available.";
         }
 
-        return "The book \"" + book + "\" is currently unavailable.";
+        return book + " is currently borrowed.";
+    }
+
+    @PostMapping("/borrow")
+    public String borrow(@RequestParam String book){
+
+        if(available){
+            available = false;
+            return "You have borrowed " + book;
+        }
+
+        return book + " is currently unavailable.";
+    }
+
+    @PutMapping("/return")
+    public String returnBook(@RequestParam String book){
+
+        available = true;
+        return "Thank you for returning " + book;
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteBook(@RequestParam String book){
+
+        return book + " has been removed from the library.";
     }
 }
