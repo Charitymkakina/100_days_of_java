9494c4e63231dfd0b88496a9826e3abafcf7d3cb
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-25 21:56:24 +0300

    API

---
+++ demo/src/main/java/com/example/demo/controllers/LibraryController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.libraryapi.models.Author;
+import com.example.libraryapi.models.Book;
+
+@RestController
+@RequestMapping("/library")
+public class LibraryController {
+
+    private List<Book> books = new ArrayList<>();
+    private List<Author> authors = new ArrayList<>();
+
+    public LibraryController() {
+
+        Author author1 = new Author(1, "George Orwell");
+        Author author2 = new Author(2, "J.K. Rowling");
+
+        authors.add(author1);
+        authors.add(author2);
+
+        books.add(new Book(101, "1984", author1));
+        books.add(new Book(102, "Harry Potter", author2));
+
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return books;
+    }
+     @GetMapping("/authors")
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    @PostMapping("/borrow")
+    public String borrowBook(@RequestParam String title) {
+
+        return "You have borrowed \"" + title + "\"";
+    }
+
+    @PutMapping("/return")
+    public String returnBook(@RequestParam String title) {
+
+        return "You have returned \"" + title + "\"";
+    }
+
+    @DeleteMapping("/remove")
+    public String removeBook(@RequestParam String title) {
+
+        return "\"" + title + "\" has been removed from the library.";
+    }
+
+}
