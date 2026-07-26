9183e2a8c6f4c65a025c59c8e76e1ab9e1340877
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-26 12:49:02 +0300

    Service

---
+++ demo/src/main/java/com/example/demo/Service/LibraryService.java
@@ -0,0 +1,7 @@
+package com.example.demo.Service;
+
+public class LibraryService {
+    public String borrowBook(String title) {
+        return "You have borrowed *" + title + "* successfully.";
+    }
+}
