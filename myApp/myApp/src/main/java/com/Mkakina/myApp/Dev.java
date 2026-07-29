83d33b3e7614b9892322acbb958a29289643e2d7
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-29 12:43:47 +0300

    Dependency Injection Using SpringBoot

--- myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
+++ myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
@@ -1,6 +1,10 @@
 package com.Mkakina.myApp;
 
-public class Dev {
+import org.springframework.stereotype.Component;
+
+@Component // Shows this is the class i want to manage
+
+public class Dev { //Managed bean therfore creates an object for you in the container
     public void build(){
         System.out.println("Working on building my Java skills");
     }
