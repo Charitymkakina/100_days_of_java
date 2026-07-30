784c31e91e38a063b90c13149dfa668401aabd32
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-30 09:54:13 +0300

    Autowiring

--- myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
+++ myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
@@ -1,13 +1,15 @@
 package com.Mkakina.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component // Shows this is the class i want to manage
 
 public class Dev { //Managed bean therfore creates an object for you in the container
 
+    @Autowired // connecting dev and laptop.
     private Laptop laptop;
-    
+
     public void build(){
 
         laptop.compile();
--- myApp/myApp/src/main/java/com/Mkakina/myApp/Laptop.java
+++ myApp/myApp/src/main/java/com/Mkakina/myApp/Laptop.java
@@ -1,5 +1,8 @@
 package com.Mkakina.myApp;
 
+import org.springframework.stereotype.Component;
+
+@Component // The object is now visible in the spring container.
 public class Laptop {
     public void compile(){
         System.out.println("Still learning SpringBoot");
