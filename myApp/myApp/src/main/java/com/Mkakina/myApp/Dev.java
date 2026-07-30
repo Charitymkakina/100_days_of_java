811d176c402902ee2f349cfa6784a2882a46bf2a
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-30 09:48:26 +0300

    Autowiring

--- myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
+++ myApp/myApp/src/main/java/com/Mkakina/myApp/Dev.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Component;
 @Component // Shows this is the class i want to manage
 
 public class Dev { //Managed bean therfore creates an object for you in the container
+
+    private Laptop laptop;
     public void build(){
+
+        laptop.compile();
         System.out.println("Working on building my Java skills");
     }
 }
---
+++ myApp/myApp/src/main/java/com/Mkakina/myApp/Laptop.java
@@ -0,0 +1,7 @@
+package com.Mkakina.myApp;
+
+public class Laptop {
+    public void compile(){
+        System.out.println("Still learning SpringBoot");
+    }
+}
