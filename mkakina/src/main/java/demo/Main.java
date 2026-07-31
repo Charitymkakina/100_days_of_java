9ea01387a1a18443595167be41506757fa8559fb
Charity Mkakina <charitymkakina1@gmail.com>
2026-07-31 13:12:31 +0300

    Spring without boot

--- mkakina/src/main/java/demo/Main.java
+++ mkakina/src/main/java/demo/Main.java
@@ -1,13 +1,14 @@
 package demo;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+try (ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext("applicationContext.xml")) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Dev dev = context.getBean(Dev.class);
-        dev.build();
+    Dev dev = context.getBean(Dev.class);
+    dev.build();
+}
     }
 }
