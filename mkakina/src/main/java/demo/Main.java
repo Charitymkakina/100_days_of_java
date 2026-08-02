336f8392e5b6c52981ff91061e74c2e554bbc2b1
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-02 21:27:59 +0300

    Xml configuration

--- mkakina/src/main/java/demo/Main.java
+++ mkakina/src/main/java/demo/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 try (ClassPathXmlApplicationContext context =
-        new ClassPathXmlApplicationContext("applicationContext.xml")) {
+        new ClassPathXmlApplicationContext("applicationContext.xml")) { //Xml configuration
 
     Dev dev = context.getBean(Dev.class);
     dev.build();
