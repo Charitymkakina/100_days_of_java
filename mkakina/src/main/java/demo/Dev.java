8702f1c83d9782eef08cf7bce893de4b93ec1ca8
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-03 16:24:14 +0300

    Spring XML configuration

--- mkakina/src/main/java/demo/Dev.java
+++ mkakina/src/main/java/demo/Dev.java
@@ -1,6 +1,11 @@
 package demo;
 
 public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+
     public void build (){
         System.out.println("Using spring without boot");
     }
---
+++ mkakina/src/main/java/demo/Laptop.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop Constructor");
+    }
+
+
+    public void compile(){
+        System.out.println("I am compling my code!");
+    }
+}
