fea73ee11d30fb3a5b33baa272f70d039e796255
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-04 19:01:53 +0300

    Setter Injection

--- src/Basics/Day40/Dev.java
+++ src/Basics/Day40/Dev.java
@@ -1,10 +1,15 @@
 package Basics.Day40;
 
 public class Dev {
-   private Laptop laptop = new Laptop(); // we have created the laptop object
+    private Laptop laptop;
+// private Laptop laptop = new Laptop(); // we have created the laptop object
    
+   public void setLaptop(Laptop laptop){
+    this.laptop = laptop;
+   }
+    
    public void build(){
     laptop.compile(); // we have called the laptop 
-    System.out.println("Building Java Projects");
+   // System.out.println("Building Java Projects");
    }
 }
--- src/Basics/Day40/Main.java
+++ src/Basics/Day40/Main.java
@@ -1,11 +1,14 @@
 package Basics.Day40;
 
+// Main creates the laptop, creates the Dev and  dev uses the laptop making setter injection
 public class Main {
     public static void main(String[] args) {
 
+        Laptop laptop = new Laptop();
         Dev dev = new Dev();
 
-        laptop.compile();
+        dev.setLaptop(laptop); 
+        //laptop.compile();
         dev.build();
     }
 }
