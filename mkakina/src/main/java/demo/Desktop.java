bdd320df35180a220ec3189c9bb8ef95941295c4
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-05 15:20:55 +0300

    Autowiring in Spring

---
+++ mkakina/src/main/java/demo/Desktop.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Desktop {
+    public Desktop(){
+        System.out.println("Desktop Constructor");
+    }
+
+
+    public void compile(){
+        System.out.println("I am compling my code!");
+    }
+}
