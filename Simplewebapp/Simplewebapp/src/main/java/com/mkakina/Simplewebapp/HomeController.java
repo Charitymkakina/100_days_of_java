ba2bdc71256000e5a5055edaddb713f7e1ba4f12
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-14 13:21:54 +0300

    Web app building

--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/HomeController.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/HomeController.java
@@ -2,11 +2,13 @@ package com.mkakina.Simplewebapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @RequestMapping ("/")
+    @ResponseBody
     public String greet(){
         return "Welcome to my Web App building journey";
     }
