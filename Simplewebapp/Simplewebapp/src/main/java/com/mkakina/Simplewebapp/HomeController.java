bc07d37600732ec78f2aa01bc675bc948cbb5b48
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-14 13:26:07 +0300

    RestController

--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/HomeController.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/HomeController.java
@@ -1,14 +1,14 @@
 package com.mkakina.Simplewebapp;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping ("/")
-    @ResponseBody
     public String greet(){
         return "Welcome to my Web App building journey";
     }
