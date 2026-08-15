457f7d60a30ba6d61b192f67ce35a8881cd1ffca
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-15 22:13:56 +0300

    Controller, Service, Model

--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/model/Product.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/model/Product.java
@@ -1,8 +1,10 @@
 package com.mkakina.Simplewebapp.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Product {
     private int proId;
     private String prodName;
--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/service/ProductService.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/service/ProductService.java
@@ -1,5 +1,12 @@
 package com.mkakina.Simplewebapp.service;
 
+import java.util.List;
+
+import com.mkakina.Simplewebapp.model.Product;
+
 public class ProductService {
-    
+    List <Product> products = Arrays.asList(new Product(101, "Iphone", 50000) new Product(102,"Samsung",45000)) 
+    public List<Product> getProduct(){
+        return null;
+    }
 }
