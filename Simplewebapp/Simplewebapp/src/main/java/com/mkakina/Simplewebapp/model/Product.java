6a5b67811e28f2cd356ccffb83cff087fff6895b
Charity Mkakina <charitymkakina1@gmail.com>
2026-08-16 10:59:10 +0300

    Debugging

--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/model/Product.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/model/Product.java
@@ -1,10 +1,13 @@
 package com.mkakina.Simplewebapp.model;
 
+import org.springframework.stereotype.Component;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Component
 public class Product {
     private int proId;
     private String prodName;
--- Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/service/ProductService.java
+++ Simplewebapp/Simplewebapp/src/main/java/com/mkakina/Simplewebapp/service/ProductService.java
@@ -2,10 +2,15 @@ package com.mkakina.Simplewebapp.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.mkakina.Simplewebapp.model.Product;
 
+@Service
 public class ProductService {
-    List <Product> products = Arrays.asList(new Product(101, "Iphone", 50000) new Product(102,"Samsung",45000)) 
+    List <Product> products = Arrays.asList(
+        new Product(101, "Iphone", 50000), 
+        new Product(102,"Samsung",45000));
     public List<Product> getProduct(){
         return null;
     }
